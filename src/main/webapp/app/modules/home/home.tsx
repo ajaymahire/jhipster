57c1d1f07fb96b13c7cf7089fcac198db9c52399
rahul <rahul.k@mitlag.com>
2020-12-23 19:42:45 +0530

    Changed to home to login

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -1,109 +1,105 @@
-import './home.scss';
-
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Translate } from 'react-jhipster';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Row, Col, Alert } from 'reactstrap';
-
+import { Link, Redirect, RouteComponentProps } from 'react-router-dom';
+import { Translate, translate } from 'react-jhipster';
 import { IRootState } from 'app/shared/reducers';
+import { login } from 'app/shared/reducers/authentication';
+import { Row, Col, Label, Alert, Button } from 'reactstrap';
+import { AvForm, AvField, AvGroup, AvInput } from 'availity-reactstrap-validation';
 
-export type IHomeProp = StateProps;
+export interface ILoginProps extends StateProps, DispatchProps, RouteComponentProps<{}> {}
 
-export const Home = (props: IHomeProp) => {
-  const { account } = props;
+export const Home = (props: ILoginProps) => {
+  const handleLogin = (event, form, values) => {
+    props.login(values.username, values.password, values.rememberMe);
+  }
 
+  const { location, isAuthenticated } = props;
+  const { from } = (location.state as any) || { from: { pathname: '/', search: location.search } };
+  if (isAuthenticated) {
+    return <Redirect to={from} />;
+  }
   return (
-    <Row>
-      <Col md="9">
-        <h2>
-          <Translate contentKey="home.title">Welcome, Java Hipster!</Translate>
-        </h2>
-        <p className="lead">
-          <Translate contentKey="home.subtitle">This is your homepage</Translate>
-        </p>
-        {account && account.login ? (
-          <div>
-            <Alert color="success">
-              <Translate contentKey="home.logged.message" interpolate={{ username: account.login }}>
-                You are logged in as user {account.login}.
-              </Translate>
-            </Alert>
-          </div>
-        ) : (
-          <div>
+    <div>
+      <Row className="justify-content-center">
+        <Col md="8">
+          <h1 id="login-title">
+            <Translate contentKey="login.title">Sign in</Translate>
+          </h1>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col md="8">
+          <AvForm onSubmit={handleLogin}>
+            <Row>
+              <Col md="12">
+                {props.loginError ? (
+                  <Alert color="danger">
+                    <Translate contentKey="login.messages.error.authentication">
+                      <strong>Failed to sign in!</strong> Please check your credentials and try again.
+                    </Translate>
+                  </Alert>
+                ) : null}
+              </Col>
+              <Col md="12">
+                <AvField
+                  name="username"
+                  label={translate('global.form.username.label')}
+                  placeholder={translate('global.form.username.placeholder')}
+                  required
+                  errorMessage="Username cannot be empty!"
+                  autoFocus
+                />
+                <AvField
+                  name="password"
+                  type="password"
+                  label={translate('login.form.password')}
+                  placeholder={translate('login.form.password.placeholder')}
+                  required
+                  errorMessage="Password cannot be empty!"
+                />
+                <AvGroup check inline>
+                  <Label className="form-check-label">
+                    <AvInput type="checkbox" name="rememberMe" /> <Translate contentKey="login.form.rememberme">Remember me</Translate>
+                  </Label>
+                </AvGroup>
+              </Col>
+              <Col>
+                <Button id="login-submit" color="primary" type="submit">
+                  <Translate contentKey="login.form.button">Sign in</Translate>
+                </Button>
+              </Col>
+            </Row>
+            <div className="mt-1">&nbsp;</div>
             <Alert color="warning">
-              <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
-              <Link to="/login" className="alert-link">
-                <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
+              <Link to="/account/reset/request">
+                <Translate contentKey="login.password.forgot">Did you forget your password?</Translate>
               </Link>
-              <Translate contentKey="global.messages.info.authenticated.suffix">
-                , you can try the default accounts:
-                <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
-                <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
-              </Translate>
             </Alert>
-
             <Alert color="warning">
-              <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
-              <Link to="/account/register" className="alert-link">
+              <span>
+                <Translate contentKey="global.messages.info.register.noaccount">You don&apos;t have an account yet?</Translate>
+              </span>{' '}
+              <Link to="/account/register">
                 <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
               </Link>
             </Alert>
-          </div>
-        )}
-        <p>
-          <Translate contentKey="home.question">If you have any question on JHipster:</Translate>
-        </p>
-
-        <ul>
-          <li>
-            <a href="https://www.jhipster.tech/" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.homepage">JHipster homepage</Translate>
-            </a>
-          </li>
-          <li>
-            <a href="http://stackoverflow.com/tags/jhipster/info" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.stackoverflow">JHipster on Stack Overflow</Translate>
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/jhipster/generator-jhipster/issues?state=open" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.bugtracker">JHipster bug tracker</Translate>
-            </a>
-          </li>
-          <li>
-            <a href="https://gitter.im/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.chat">JHipster public chat room</Translate>
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/jhipster" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.follow">follow @jhipster on Twitter</Translate>
-            </a>
-          </li>
-        </ul>
-
-        <p>
-          <Translate contentKey="home.like">If you like JHipster, do not forget to give us a star on</Translate>{' '}
-          <a href="https://github.com/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-            Github
-          </a>
-          !
-        </p>
-      </Col>
-      <Col md="3" className="pad">
-        <span className="hipster rounded" />
-      </Col>
-    </Row>
+        </AvForm>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
-const mapStateToProps = storeState => ({
-  account: storeState.authentication.account,
-  isAuthenticated: storeState.authentication.isAuthenticated,
+const mapStateToProps = ({ authentication }: IRootState) => ({
+  isAuthenticated: authentication.isAuthenticated,
+  loginError: authentication.loginError,
+  showModal: authentication.showModalLogin,
 });
 
-type StateProps = ReturnType<typeof mapStateToProps>;
+const mapDispatchToProps = { login };
 
-export default connect(mapStateToProps)(Home);
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
